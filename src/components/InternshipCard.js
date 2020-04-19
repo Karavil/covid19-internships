54c81e1b55fdf8db0a82409fb6ff12c10dffb4b4
Alex Karavil <46430499+Karavil@users.noreply.github.com>
2020-04-19 18:42:41 -0400

    Got rid of unused code

--- src/components/InternshipCard.js
+++ src/components/InternshipCard.js
@@ -9,17 +9,6 @@ import {
 } from "@chakra-ui/core";
 
 const InternshipCard = ({ internship }) => {
-   const property = {
-      imageUrl: "https://bit.ly/2Z4KKcF",
-      imageAlt: "Rear view of modern home with pool",
-      beds: 3,
-      baths: 2,
-      title: "Modern home in city center in the heart of historic Los Angeles",
-      formattedPrice: "$1,900.00",
-      reviewCount: 34,
-      rating: 4,
-   };
-
    const StatusData = {
       status: internship.cancelled ? "error" : "success",
       title: internship.cancelled ? "Cancelled" : "Still on!",
